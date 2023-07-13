95bc916d8d72db9a8c4abe01fdbd9df129d7e1b2
PickledCucumber <sebastian.romejko@zhp.net.pl>
2023-07-14 00:04:08 +0200

    Added collisions and rotating

--- destructible_object.gd
+++ destructible_object.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@export var timeout = 100
+@export var collide = false
+@export var force = 5000
+@export var timeout = 5
 @export var gravity = 0.7
 @export var scale_modifier = 0.0
 @export var width_limit = 8.0
@@ -10,60 +12,28 @@ const FRAGMENT_PATH = "res://object_fragment.tscn"
 
 var exploded = false
 var fragments = []
-var drops = []
-var frames_number = 0
+var frames = 0
 var timer = 0
 var obj_height
 var obj_width
 var obj_animations_frames_h
 var obj_animations_frames_v
-
-class Fragmentation:
-	var old
-	var new
-	
-	func _init(arg1, arg2):
-		old = Vector2(arg1, arg2)
-		new = old
-		
-	func integrate():
-		var velocity = velocity()
-		old = new
-		new += velocity
-		
-	func velocity():
-		return new - old
-		
-	func move(offset_x, offset_y):
-		new += Vector2(offset_x, offset_y)
-		
-	func get_position():
-		return new
 			
 func _ready():
 	init()
 	set_to_default()
-	set_process_input(true);
-	set_process(true)
 				
 func _process(delta):
 	if exploded:
-		for i in range(drops.size()):
-			drops[i].move(0, gravity)
-			drops[i].integrate()
-			fragments[i].set_position(Vector2(drops[i].get_position().x, drops[i].get_position().y))
-			fragments[i].set_scale(Vector2(max(1, scale_modifier * timer), max(1, scale_modifier * timer)))
+		for fragment in fragments:
+			if scale_modifier != 0:
+				fragment.set_scale(Vector2(max(1, scale_modifier * timer), max(1, scale_modifier * timer)))
 
-			if fragments[i].position.x < 0:
-				fragments[i].rotate(randf_range(-0.01, -0.03))
-			if fragments[i].position.x > 0:
-				fragments[i].rotate(randf_range(0.01, 0.3))
-				
-		timer += 1
-			
+		timer += delta
 		if timer > timeout:
-			drops.clear()
 			clear()
+			init()
+			set_to_default()
 			timer = 0
 			exploded = false
 			
@@ -78,36 +48,56 @@ func init():
 	obj_width = obj_fragment_temp.texture.get_size().x
 	obj_animations_frames_h = obj_fragment_temp.hframes
 	obj_animations_frames_v = obj_fragment_temp.vframes
-	frames_number = obj_animations_frames_h * obj_animations_frames_v
+	frames = obj_animations_frames_h * obj_animations_frames_v
 	obj_fragment_temp.queue_free()
 
-	for n in range (frames_number):
-		fragments.append(objNode.instantiate() as Sprite2D)
-		fragments[n].set_frame(n)
-		add_child(fragments[n])
+	for n in range(frames):
+		var rigid_body = RigidBody2D.new()
+		add_child(rigid_body)
+		rigid_body.freeze = true
+		rigid_body.gravity_scale = gravity
+		
+		var sprite = objNode.instantiate() as Sprite2D
+		rigid_body.add_child(sprite)
+		sprite.set_frame(n)
+
+		var collision_shape = CollisionShape2D.new()
+		rigid_body.add_child(collision_shape)
+		collision_shape.shape = RectangleShape2D.new()
+		collision_shape.shape.size = Vector2(obj_width/obj_animations_frames_h, obj_height/obj_animations_frames_v)
+		if !collide:
+			collision_shape.disabled = true
+		
+		fragments.append(rigid_body)
 
 func set_to_default():
 	var frame = 0
 	for x in range(obj_animations_frames_v):
 		for y in range(obj_animations_frames_h):
 			fragments[frame].set_position(Vector2(y*(obj_width/obj_animations_frames_h), x*(obj_height/obj_animations_frames_v)))
-			fragments[frame].set_scale(Vector2(1, 1))
 			fragments[frame].set_rotation(0)
 			frame += 1
 
 func explode():
-	if drops.size() < fragments.size():
-		for j in range (fragments.size()):
-			var drop = Fragmentation.new(fragments[j].position.x, fragments[j].position.y)
-			drop.move(randf_range(-width_limit, width_limit), randf()*(-height_limit)-10)
-			drops.push_front(drop)
-	
-	exploded = true
-    
+	for fragment in fragments:
+		fragment.freeze = false
+		var force_to_apply = Vector2(randf_range(-width_limit, width_limit), randf()*(-height_limit)-10) * force
+		
+		fragment.apply_force(force_to_apply)
 
+		#fragments.apply_torque_impulse(randf_range(-1, -5))
+		
+		fragment.set_inertia(1)
+		if force_to_apply.x < 0:
+			fragment.apply_torque_impulse(randf_range(-1, -5))
+		if force_to_apply.x > 0:
+			fragment.apply_torque_impulse(randf_range(1, 5))
+		fragment.set_inertia(0)
+
+	exploded = true
 
 func clear():
-	for n in range(frames_number):
+	for n in range(frames):
 		remove_child(fragments[n])
 			
 	fragments.clear()
